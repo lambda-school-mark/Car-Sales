61da36f7ddab2adad85d2fb2958ad1925b4eb319
Mark Pekun <markpekun@marks-mbp.lan>
2020-07-14 15:35:50 -0400

    createStore and provider from redux; store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-import 'bulma/css/bulma.css';
-import './styles.scss';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+import "bulma/css/bulma.css";
+import "./styles.scss";
+
+const store = createStore();
+console.log("this is my state:", store.getState());
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
