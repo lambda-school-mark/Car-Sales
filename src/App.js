0d31fb4c97e27bebf2b22eb623b461ccd4dd8a9f
Mark Pekun <markpekun@marks-mbp.lan>
2020-07-14 15:48:22 -0400

    made mapStateToProps

--- src/App.js
+++ src/App.js
@@ -4,21 +4,29 @@ import Header from "./components/Header";
 import AddedFeatures from "./components/AddedFeatures";
 import AdditionalFeatures from "./components/AdditionalFeatures";
 import Total from "./components/Total";
-import { connect } from "redux";
+import { connect } from "react-redux";
 
-const App = () => {
+const App = (props) => {
   return (
     <div className="boxes">
       <div className="box">
-        <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+        <Header car={props.car} />
+        <AddedFeatures car={props.car} />
       </div>
       <div className="box">
         <AdditionalFeatures additionalFeatures={state.additionalFeatures} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
+        <Total car={props.car} additionalPrice={state.additionalPrice} />
       </div>
     </div>
   );
 };
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    additionalPrice: state.additionalFeatures,
+    additionalFeatures: state.additionalFeatures,
+    car: state.car,
+  };
+};
+
+export default connect(null, {})(App);
